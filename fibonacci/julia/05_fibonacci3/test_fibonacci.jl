4ee6518ba3f0d56431126993a1dcc47424658bdb
Sora Shiratani <hclfuranpon_compotayamano_0117@outlook.jp>
2023-03-08 02:06:29 +0900

    Format by JuliaFormatter.jl (default settings)

--- fibonacci/julia/05_fibonacci3/test_fibonacci.jl
+++ fibonacci/julia/05_fibonacci3/test_fibonacci.jl
@@ -1,12 +1,7 @@
 using Test
 include("fibonacci.jl")
 
-const TESTCASES = (
-    (0, 0),
-    (1, 1),
-    (2, 1),
-    (3, 2),
-)
+const TESTCASES = ((0, 0), (1, 1), (2, 1), (3, 2))
 
 @testset "fibonacci" begin
     for (n, v) ∈ TESTCASES
--- fibonacci/julia/08_linear/test_fibonacci.jl
+++ fibonacci/julia/08_linear/test_fibonacci.jl
@@ -1,7 +1,8 @@
 using Test
 include("fibonacci.jl")
 
-const TESTCASES = ((-2, -1), (-1, 1), (0, 0), (1, 1), (2, 1), (3, 2), (10, 55), (40, 102334155))
+const TESTCASES =
+    ((-2, -1), (-1, 1), (0, 0), (1, 1), (2, 1), (3, 2), (10, 55), (40, 102334155))
 
 @testset "fibonacci" begin
     for (n, v) ∈ TESTCASES
